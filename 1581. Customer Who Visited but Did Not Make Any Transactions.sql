63163bd103b030f4f96c640f955d6c064040d119
Revanth Karumajji <revanthanil408@gmail.com>
2026-01-12 22:36:46 -0500

    eighth problem

---
+++ 1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,7 @@
+Select Visits.customer_id,
+Count(Visits.customer_id) as count_no_trans
+from Visits
+left join Transactions
+on Visits.visit_id = Transactions.visit_id
+where Transactions.transaction_id is NULL
+group by Visits.customer_id
