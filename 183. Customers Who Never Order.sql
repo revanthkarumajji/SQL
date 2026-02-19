51862d06434096e090e16b04e09c2763b25b8886
Revanth Karumajji <revanthanil408@gmail.com>
2026-02-18 23:59:21 -0500

    general

---
+++ 183. Customers Who Never Order.sql
@@ -0,0 +1,6 @@
+Select 
+c.name as Customers 
+from Customers c
+left join Orders o
+on c.id = o.customerId
+where o.customerId is null
---
+++ 619. Biggest Single Number.sql
@@ -0,0 +1,7 @@
+Select  Max(num) as num 
+from
+(Select 
+num
+from MyNumbers
+group by num 
+having count(num) = 1) d
